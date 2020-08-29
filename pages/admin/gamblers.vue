a04d1a31edb3c325ea61885c43d08ef25b3ddafb
YurijM <y-myagkov@mail.ru>
2020-08-29 22:04:43 +0300

    Добавлена форма редактирования игрока в админке

--- pages/admin/gamblers.vue
+++ pages/admin/gamblers.vue
@@ -2,6 +2,38 @@
   <div>
     <mu-title-page-admin title="Игроки"/>
 
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">
+            {{`${editedItem.family} ${editedItem.name} (${editedItem.nickname})`}}
+          </span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="editedItem.nickname" label="Ник"/>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="editedItem.family" label="Фамилия"/>
+              </v-col>
+              <v-col cols="12" sm="6" md="4">
+                <v-text-field v-model="editedItem.name" label="Имя"/>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="close">Отмена</v-btn>
+          <v-btn color="blue darken-1" text @click="save">Сохранить</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
     <v-data-table
       dense
       :headers="headers"
@@ -15,7 +47,7 @@
       pageText: '{0}-{1} из {2}'
     }"
     >
-      <template v-slot:item.actions="{ item }">
+      <template v-slot:item.actions="{item}">
         <v-icon
           small
           class="mr-2"
@@ -40,12 +72,30 @@
     },
     data() {
       return {
+        dialog: false,
         headers: [
           {text: 'Ник', value: 'nickname'},
           {text: 'Фамилия', value: 'family'},
-          {text: 'Имя', value: 'name'}
+          {text: 'Имя', value: 'name'},
+          {text: '', value: 'actions', sortable: false }
         ],
+        editedIndex: -1,
+        editedItem: {
+          nickname: '',
+          family: '',
+          name: ''
+        },
+        defaultItem: {
+          nickname: '',
+          family: '',
+          name: ''
+        },
       }
+    },
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
     },
     computed: {
       ...mapGetters({
@@ -54,6 +104,28 @@
       gamblers() {
         return this.getGamblersOrderByNick
       }
+    },
+    methods: {
+      editItem (item) {
+        this.editedIndex = this.gamblers.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.gamblers[this.editedIndex], this.editedItem)
+        } else {
+          this.gamblers.push(this.editedItem)
+        }
+        this.close()
+      },
     }
   }
 </script>
