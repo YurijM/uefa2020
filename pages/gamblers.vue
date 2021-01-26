4de1ccda0c975f130d4d93c4309b31914b9d39fc
YurijM <y-myagkov@mail.ru>
2021-01-26 21:12:31 +0300

    Если участник зарегистрировался, но ещё не оплатил участие, то на странице "Участники" у НЕГО в браузере будет показано его "пустое" фото.

--- pages/gamblers.vue
+++ pages/gamblers.vue
@@ -3,12 +3,44 @@
     <h2 class="text-center my-2 purple--text">Участники</h2>
     <div class="d-flex flex-wrap flex-row justify-center px-1">
       <v-card
+        v-if="getGambler.status === 1"
+        max-width="125"
+        outlined
+        class="mb-1 dark blue-grey lighten-4"
+        :style="{border: '1px solid ' + (getGambler.sex === 'ж' ? 'red' : 'blue') + ' !important'}"
+      >
+        <v-img
+          lazy-src="/user.jpg"
+          src="/user.jpg"
+          class="white--text align-end"
+        >
+          <template v-slot:placeholder>
+            <v-row
+              class="fill-height ma-0"
+              align="center"
+              justify="center"
+            >
+              <v-progress-circular indeterminate color="black"></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+
+        <v-card-title
+          class="pa-1 flex-column caption text-center"
+          style="word-break: normal !important"
+          :style="{borderTop: '1px solid ' + (getGambler.sex === 'ж' ? 'red' : 'blue') + ' !important'}"
+        >
+          <div>После оплаты участия здесь будет Ваше фото</div>
+        </v-card-title>
+      </v-card>
+
+      <v-card
         v-for="(gambler, i) in gamblers"
         :key="gambler.id"
         max-width="125"
         outlined
         class="mb-1 dark blue-grey lighten-4"
-        :class="i > 0 ? 'ml-1' : ''"
+        :class="(i > 0 || getGambler.status === 1) ? 'ml-1' : ''"
         :style="{border: '1px solid ' + (gambler.sex === 'ж' ? 'red' : 'blue') + ' !important'}"
       >
         <v-img
@@ -47,7 +79,8 @@
     name: 'gambler',
     computed: {
       ...mapGetters({
-        getGamblersByName: 'gambler/getGamblersByName'
+        getGamblersByName: 'gambler/getGamblersByName',
+        getGambler: 'gambler/getGambler'
       }),
       gamblers() {
         return this.getGamblersByName
